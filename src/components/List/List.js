70d22392d01caff50ef56703bb41ac47a885eb31
Akshay Dodkade <akshaydodkade@gmail.com>
2021-09-15 02:50:02 +0530

    Board - Added List and Title Component

---
+++ src/components/List/List.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { CssBaseline, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Title from "./Title";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    width: '300px',
+    backgroundColor: '#EBECF0',
+    marginLeft: theme.spacing(1)
+  }
+}));
+
+export default function List() {
+  const classes = useStyle();
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <CssBaseline/>
+        <Title />
+      </Paper>
+    </div>
+  )
+}
